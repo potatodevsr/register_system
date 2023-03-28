557fb0d45f4a5d01cc15c61f9879a30026476591
potatodevs <hoshiku1997@gmail.com>
2023-03-28 16:10:51 +0700

    register-login

--- login_db.php
+++ login_db.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
     session_start();
     include('server.php');
 
@@ -11,13 +11,13 @@
         if (empty($username)) {
             array_push($errors, "Username is required");
         }
-        
+
         if (empty($password)) {
-            array_push($errors, "Passwprd is required");
+            array_push($errors, "Password is required");
         }
 
         if (count($errors) == 0) {
-            $password =md5($password);
+            $password = md5($password);
             $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password' ";
             $result = mysqli_query($conn, $query);
 
@@ -26,14 +26,15 @@
                 $_SESSION['success'] = "Your are now logged in";
                 header("location: index.php");
             } else {
-                array_push($errors, "Wrong username or Password");
-                $_SESSION['error'] = "Wrong username or Password!";
-                header("location: login.php");
-            } 
-             } else {
-                array_push($errors, "Username & Password is required");
-                $_SESSION['error'] = "Wrong username or Password!";
+                array_push($errors, "Wrong Username or Password");
+                $_SESSION['error'] = "Wrong Username or Password!";
                 header("location: login.php");
             }
+        } else {
+            array_push($errors, "Username & Password is required");
+            $_SESSION['error'] = "Username & Password is required";
+            header("location: login.php");
         }
+    }
+
 ?>
